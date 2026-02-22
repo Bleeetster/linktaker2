40efec803995bd6d10f4f0c0ff8cc246a3c99807
Bleeetster <vedjadkov@gmail.com>
2026-02-23 02:46:22 +0300

    partialy implemented saveLink module

--- modules/saveLink.js
+++ modules/saveLink.js
@@ -1,18 +1,24 @@
 class IsaveLink {
-	constructor (logger, errorHandler) {
+	constructor (logger, errorHandler, storage) {
 		this.logger = logger;
 		this.errorHandler = errorHandler;
+		this.storage = storage;
 	}
 	async saveUrls() { throw new Error("Not implemented")}
 	async saveUrl() { throw new Error("Not implemented")}
 	async queryTabs() { throw new Error("Not implemented")}
 }
 export class saveUrl extends IsaveLink {
-	async queryTabs() {
-		const queryOptions = { lastFocusedWindow: true };
-		let [ tabs ] = await chrome.tabs.query(queryOptions);
-		console.log(tabs);
-		this.logger.log(tabs)
+	async saveUrl() {
+		try {
+			const tab = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+			const output = {title: tab[0].title, url: tab[0].url};
+			await this.storage.add(output);
+		} 
+		catch (err) {
+			console.error(err)
+			this.errorHandler.logError(err)
+		}
 	}
 }
 
