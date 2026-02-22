5a54b7289d33bea6338cac05e812a8e98c8f486f
Bleeetster <vedjadkov@gmail.com>
2026-02-22 21:08:57 +0300

    partialy implemented storage module

--- modules/storage.js
+++ modules/storage.js
@@ -2,12 +2,44 @@ export class iStorage {
 	constructor( logger, errorhandler) {
 		this.logger = logger;
 		this.errorhandler = errorhandler;
+
+		this.observe();
 	}
-	async push(key, value) { throw new Error('Storage is not implemented') }
+	async add(...urls) { throw new Error('Storage is not implemented') }
+	async insert(id, url) { throw new Error('Storage is not implemented') }
 	async get(key) { throw new Error('Storage is not implemented') }
 	async remove(key) { throw new Error('Storage is not implemented') }
 	async clear() { throw new Error('Storage is not implemented') }
+	async observe() { throw new Error('Storage is not implemented') }
 }
 
 export class Storage extends iStorage {
+	// async observe() {
+	// 	chrome.storage.onChanged.addListener( (changes, namespace) => {
+	// 	})
+	// }
+	async add(...urls) {
+		urls.forEach( async (url) => {
+			const len = await chrome.storage.local.getKeys()
+			len = len.length
+			await chrome.storage.local.set({[len+1]: url });
+		})
+	}
+// 	async remove(...keys) {
+// 		await chrome.storage.local.remove(keys.map((key) => String(key)));
+// 	}
+// 	usefull function might use later
+// 	async switch(key1, key2) {
+// 		console.log('test');
+// 		key1 = new String(key1);
+// 		key2 = new String(key2);
+// 		console.log(`switching ${key1} and ${key2}`)
+// 		const value1 = await chrome.storage.local.get(key1);
+// 		const value2 = await chrome.storage.local.get(key2);
+// 		await chrome.storage.local.set({[key1]: value2});
+// 		await chrome.storage.local.set({[key2]: value1});
+// 	}
+// 	async clear() {
+// 		await chrome.storage.local.clear();
+// 	}
 }
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,13 @@
 import { saveUrl } from './modules/saveLink.js'
-import { iDownloader } from './modules/downloader.js'
-import { iStorage } from './modules/storage.js' 
+import { Downloader } from './modules/downloader.js'
+import { Storage } from './modules/storage.js' 
 import { mainController } from './modules/controller.js'
 import { Logger, ErrorHandler } from './modules/logger.js'
 
 const logger = new Logger()
 const errorHandler = new ErrorHandler()
 const saver = new saveUrl(logger, errorHandler)
-const downloader = new iDownloader(logger, errorHandler)
-const storage = new iStorage(logger, errorHandler)
-
+const downloader = new Downloader(logger, errorHandler)
+const storage = new Storage(logger, errorHandler)
 
 new mainController(saver, downloader, storage)
-
-
