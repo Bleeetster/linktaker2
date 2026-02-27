3b03345fac00b7c505bfe39d0c420977dc050bea
Bleeetster <vedjadkov@gmail.com>
2026-02-28 00:34:46 +0300

    fixed service worker

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ import { saveUrl } from './modules/saveLink.js'
 import { Downloader } from './modules/downloader.js'
 import { Storage } from './modules/storage.js' 
 import { mainController } from './modules/controller.js'
-import { Logger, ErrorHandler } from './modules/logger.js'
+import { Loger, ErrorHandler } from './modules/logger.js'
 
-const logger = new Logger()
+const loger = new Loger()
 const errorHandler = new ErrorHandler()
-const storage = new Storage(logger, errorHandler)
-const saver = new saveUrl(logger, errorHandler, storage)
-const downloader = new Downloader(logger, errorHandler, storage)
+const storage = new Storage(loger, errorHandler)
+const saver = new saveUrl(loger, errorHandler, storage)
+const downloader = new Downloader(loger, errorHandler, storage)
 
 new mainController(saver, downloader)
